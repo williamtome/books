f1f59caecb234d1f7e8be2b9e37794a92cdd1445
William Weirich Tomé <william.tomenh@gmail.com>
2026-05-29 19:13:20 -0300

    (fix) Ajuste na regra de ano de publicação e em algumas mensagens.

--- app/Http/Requests/LivroRequest.php
+++ app/Http/Requests/LivroRequest.php
@@ -23,7 +23,7 @@ class LivroRequest extends FormRequest
             'Titulo' => ['required', 'string', 'max:40'],
             'Editora' => ['required', 'string', 'max:40'],
             'Edicao' => ['required', 'numeric', 'min:1'],
-            'AnoPublicacao' => ['required', 'string', 'max:4'],
+            'AnoPublicacao' => ['required', 'numeric', 'digits:4', 'max:' . date('Y')],
         ];
     }
 
@@ -36,12 +36,13 @@ class LivroRequest extends FormRequest
             'Editora.required' => 'O campo editora é obrigatório.',
             'Editora.string' => 'O campo editora aceita somente texto.',
             'Editora.max' => 'O campo editora suporta até 40 caracteres.',
-            'AnoPublicacao.required' => 'O campo ano de publicação é obrigatório.',
-            'AnoPublicacao.string' => 'O campo ano de publicação aceita somente texto.',
-            'AnoPublicacao.max' => 'O campo ano de publicação suporta até 40 caracteres.',
             'Edicao.required' => 'O campo edição é obrigatório.',
-            'Edicao.string' => 'O campo edição aceita somente números.',
+            'Edicao.numeric' => 'O campo edição aceita somente números.',
             'Edicao.min' => 'O campo edição números acima de 1 (um).',
+            'AnoPublicacao.required' => 'O campo ano de publicação é obrigatório.',
+            'AnoPublicacao.numeric' => 'O campo ano de publicação aceita somente números.',
+            'AnoPublicacao.max' => 'O campo ano de publicação suporta até 4 caracteres.',
+            'AnoPublicacao.digits'   => 'O campo ano de publicação deve ter exatamente 4 dígitos.',
         ];
     }
 }
